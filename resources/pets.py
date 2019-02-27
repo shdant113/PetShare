aded70fd739ef3a3a33a8009ae6816c4e133cbdf
Spencer Dant <sdant@Spencers-MacBook-Pro.local>
2019-02-27 16:00:14 -0600

    pet resource api set up

--- resources/pets.py
+++ resources/pets.py
@@ -12,6 +12,14 @@ pets_fields = {
 	'special_requirements': fields.String
 }
 
+def pets_error(pet_id):
+	try:
+		pet = models.Pet.get(models.Pet.id == pet.id)
+	except models.Pet.DoesNotExist:
+		abort(404)
+	else:
+		return pet
+
 class PetList(Resource):
 	def __init__(self):
 		self.reqparse = reqparse.RequestParser()
@@ -50,3 +58,61 @@ class PetList(Resource):
 		print(arguments, ' we are hitting args')
 		parsedPet = [marshal(models.Pet.create(**arguments), pets_fields)]
 		return parsedPet
+
+class Pet(Resource):
+	def __init__(self):
+		self.reqparse = reqparse.RequestParser()
+		self.reqparse.add_argument(
+			"What is your pet's name?",
+			required = False,
+			help = 'Please provide a name',
+			location = ['form', 'json']
+		)
+		self.reqparse.add_argument(
+			"What type of animal is your pet?",
+			required = False,
+			help = 'Please tell us what type of animal your pet is',
+			location = ['form', 'json']
+		)
+		self.reqparse.add_argument(
+			"How old is your pet?",
+			required = False,
+			help = 'Please tell us how old your pet is',
+			location = ['form', 'json']
+		)
+		self.reqparse.add_argument(
+			"Are there any special requirements a sitter needs to know about in order to take care of your pet?",
+			required = False,
+			location = ['form', 'json']
+		)
+		super().__init__()
+
+	@marshal_with(pets_fields)
+	def get(self, id):
+		return pets_error
+
+	def put(self, id):
+		arguments = self.reqparse.parse_args()
+		query = models.Pet.update(**arguments).where(models.Pet.id == id)
+		query.execute()
+		parsedQuery = [marshal(models.Pet.get(models.Pet.id == id), pets_fields)]
+		return parsedQuery
+
+	def delete(self, id):
+		arguments = self.reqparse.parse_args()
+		query = models.Pet.delete().where(models.Pet.id == id)
+		query.execute()
+		return 'deleted the pet'
+
+pets_api = Blueprint('resources.pets', __name__)
+api = Api(pets_api)
+api.add_resource(
+	PetList,
+	'/pets',
+	endpoint = 'pets'
+)
+api.add_resource(
+	Pet,
+	'/pets/<int:id>',
+	endpoint = 'pet'
+)
