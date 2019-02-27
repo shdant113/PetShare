ce6820ebe52cedc6b78eeb5577cc2cc738faa1eb
Spencer Dant <sdant@Spencers-MacBook-Pro.local>
2019-02-27 14:55:21 -0600

    routing for sql

---
+++ resources/pets.py
@@ -0,0 +1,41 @@
+from flask import jsonify, Blueprint, abort
+from flask_restful import (Resource, Api, reqparse, fields, marshal,
+	marshal_with, url_for)
+from flask_login import login_required
+import models
+
+pets_fields = {
+	'id': fields.Integer,
+	'name': fields.String,
+	'pet_type': fields.String,
+	'age': fields.Integer,
+	'special_requirements': fields.String
+}
+
+class PetList(Resource):
+	def __init__(self):
+		self.reqparse = reqparse.RequestParser()
+		self.reqparse.add_argument(
+			'name',
+			required = False,
+			help = 'No pet name provided.',
+			location = ['form', 'json']
+		)
+		self.reqparse.add_argument(
+			'what kind of animal?',
+			required = False,
+			help = 'No pet type provided.',
+			location = ['form', 'json']
+		)
+		self.reqparse.add_argument(
+			'age',
+			required = False,
+			help = 'No pet age provided.',
+			location = ['form', 'json']
+		)
+		self.reqparse.add_argument(
+			'special requirements',
+			required = False,
+			location = ['form', 'json']
+		)
+		super().__init__()
